dc7ec96971442b32d7da159cc8b6aa79bf1387ef
GabrielGRCosta <galvaogabriel890@gmail.com>
2022-02-10 23:30:51 -0300

    initsteps

--- src/main/java/com/avalon/Jogar.java
+++ src/main/java/com/avalon/Jogar.java
@@ -1,2 +1,36 @@
+package com.avalon;
 
-papa;
+public class Jogar{
+
+    private int Wait,Playing,Exit;
+    private int Rounds;
+    private int ScoreBoard;
+
+    public Jogar(int Wait, int Playing,int Exit, int Rounds, int ScoreBoard){
+        this.Wait = Wait;
+        this.Playing = Playing;
+        this.Exit = Exit;
+        this.Rounds = Rounds;
+        this.ScoreBoard = ScoreBoard;
+    }
+
+    public void setRounds(int a){
+        Rounds = a;
+    }
+
+    public void setScoreBoard(int b){
+        ScoreBoard = b;
+    }
+
+    public int getRounds(){
+        return Rounds;
+    }
+
+    public int getScoreBoard(){
+        return ScoreBoard;
+    }
+
+
+
+
+}
---
+++ src/main/java/com/avalon/character.java
@@ -0,0 +1,7 @@
+package com.avalon;
+
+public abstract class character{
+
+    private String name;
+
+}
