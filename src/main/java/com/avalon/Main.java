d85c10eaa6ac0fd9bdff8d331302af8ac6eeac77
GabrielGRCosta <galvaogabriel890@gmail.com>
2022-02-13 22:05:08 -0300

    Regras

--- src/main/java/com/avalon/Main.java
+++ src/main/java/com/avalon/Main.java
@@ -26,7 +26,10 @@ public class Main {
             }
 
             else if (input == 2){
-                System.out.println("Regras");
+                System.out.println("Avalon é jogado entre 5 e 10 pessoas. E diferente de muitos jogos em que mesa cheia é sinal de jogo lento, aqui, quanto mais melhor! O grande Rei Arthur precisa consolidar seu reino e para isso, enviará seus bravos guerreiros em missões pela Bretanha. Infelizmente, o mal conseguiu se infiltrar nas fileiras de guerreiros de Arthur e os fiéis asseclas de Mordred farão todo o possível para sabotar essas missões e derrubarem o reino.\n" +
+                        "Em cada partida, serão sorteados entre os jogadores personagens fiéis a Arthur e os obedientes a Mordred, que estarão em menor número, mas terão a vantagem de se conhecerem mutuamente, sendo desconhecidos dos demais guerreiros. Esses personagens podem ser comuns ou ter alguma habilidade especial. Falarei disso mais abaixo. Cada jogador terá duas opções de voto, uma de Aprovação e outra de Recusa.\n" +
+                        "Uma vez memorizados os papeis, o jogo já pode começar. Um  jogador será nomeado como lider , que representa o líder naquela rodada, responsável por enviar a equipe..\n" +
+                        "O papel do narrador é dar a abertura e completar a fase de reconhecimento, conforme os personagens que estão em jogo. Em uma partida só com Merlin e o Assassino, ele deverá pedir que todos fechem os seus olhos e que coloquem suas mãos em punho fechado, sobre a mesa. Então ele pedirá que abram os olhos e se reconheçam somente os mínions de Mordred. Depois estes fecharão os olhos e somente Merlin abrirá os olhos. O narrador pede então que os mínions de Mordred levantem o polegar para que Merlin os conheça. Terminada essa parte, todos abrem os olhos e a 'guerra' começa.\n");
             }
 
             else if (input == 3){
